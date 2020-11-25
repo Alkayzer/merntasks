f311284614d97d077a6ebe30db5ddbf4e4698004
Marcelo <sistemas@keepsmiling.com.ar>
2020-11-25 04:57:37 -0300

    cambio en el port del server

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ app.use(cors());
 //habilitar express.json
 app.use(express.json({extended:true}));
 
-//puerto de la app
-const port = process.env.PORT || 8000;
-
 //importar rutas
 app.use('/api/usuarios',require('./routes/usuarios'));
 app.use('/api/auth',require('./routes/auth'));
@@ -24,6 +21,6 @@ app.use('/api/proyectos',require('./routes/proyectos'));
 app.use('/api/tareas',require('./routes/tareas'));
 
 //arrancar la app
-app.listen(port, '0.0.0.0', () => {
+app.listen( process.env.PORT || 4000, '0.0.0.0', () => {
     console.log(`El server esta corriendo correctamente en el puerto ${port}`);
 });
